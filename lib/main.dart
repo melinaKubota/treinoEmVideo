62ad4dec57bbe18b7fb6316c056aa43b15047eb1
Melina Kubota <melina.kubota@gmail.com>
2021-08-03 10:04:54 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'home.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+/* import 'package:firebase_core/firebase_core.dart';
+import 'home.dart'; */
 
-void main()async {
+ void main()async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -12,9 +15,31 @@ void main()async {
       .document("001")
       .setData({"Nome": "Jamilton"});
 
-  runApp(MaterialApp(
+  /* runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
-  ));
+  )); */
 }
 
+class Firestore {
+}
+
+/* 
+void main() {
+  //Inicializando o firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FirebaseStorage.instance
+    .collection("usuarios")
+    .document("pontuacao")
+    .setData({"Jamilton": "250"});
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+ */
